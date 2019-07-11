f168129e44f583abb88a579927ec799acf66957a
concon55 <clg469@nyu.edu>
2019-07-11 12:41:57 -0400

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,46 +1,53 @@
 import random
 
-# A list of words that
+# list of words
 potential_words = ["example", "words", "someone", "can", "guess"]
-
+#get a random word from the list
 word = random.choice(potential_words)
 
 # Converts the word to lowercase
 word = word.lower()
-print(word)
 
+#for testing:
+#print(word)
+
+#create an empty list 
 current_word = []
-# Make it a list of letters for someone to guess
+#populate the list with number of blanks according to size of the random word
 for i in range(len(word)):
 	current_word.append("_")
 
-# Some useful variables
+#keep track of guesses
 guesses = []
+#number of tries
 maxfails = 7
 fails = 0
 
+#for string conversion
 final = ""
 
 while fails < maxfails:
-	#get input
+	#get user input
 	print(current_word)
 	guess = input("Guess a letter: ")
 
+	#check if the user guessed the letter already (check their guesses list)
 	if(guess in guesses):
 		print("You already guessed that")
-	# check if the guess is correct: Is it in the word? If so, reveal the letters!
 	else:
-		guesses += guess
+		guesses += guess #if the letter has not been guessed, add it to the guesses list 
+		#check if the letter that the user guesses is in the word
 		if(guess in word):
-			for i in range(len(word)):
-				if(word[i] == guess):
-					current_word[i] = guess
-			if(final.join(current_word) == word):
+			for i in range(len(word)): #compare the guess to each letter in the word 
+				if(word[i] == guess): 
+					current_word[i] = guess #change the blank to the letter
+			 #if the user guesses the entire word, they win 
+			if(final.join(current_word) == word): #convert the list of letters into a string for comparison
 				print(current_word)
 				print("You win!")
 				break
-
+		#if the user guesses a wrong letter
 		else:
 			print("Nope! Guess again!")
-			fails = fails+1
+			fails = fails+1 #update counter for while loop
 		print("You have " + str(maxfails - fails) + " tries left!")
